71226f27e3da7441c1271cc392e6a2f4d8fca4de
ThroughTheNight <jmethods@163.com>
2021-11-18 23:50:10 +0800

    add: 03 匹配未定义的路由

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../home/Home";
 import Goods from "../goods/Goods";
 import Customer from "../customer/Customer";
+import NotFound from "../not-found/NotFound";
 
 export default function Router() {
   {/* 所有的路由配置均在 BrowserRouter 内部 */ }
@@ -14,6 +15,9 @@ export default function Router() {
         <Route path='/' element={<Home />} />
         <Route path='goods' element={<Goods />} />
         <Route path='customer' element={<Customer />} />
+
+        {/* 匹配未定义的路由 */}
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
